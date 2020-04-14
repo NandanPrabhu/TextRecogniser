a720645f82e5bfdc878dabfad90f9a09363b540b
nandanprabhu <nandanprabhu02@gmail.com>
2020-04-14 21:36:31 +0530

    First commit

---
+++ TextRecogniser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecogniser
+//
+//  Created by Nandan Prabhu on 14/04/20.
+//  Copyright © 2020 Nandan Prabhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
